3585a2508a01daf75dd83bbacff654279694b8a6
yahsaki <github@yahsaki.com>
2024-09-23 21:49:13 -0700

    initial

---
+++ renderer.js
@@ -0,0 +1,7 @@
+const information = document.getElementById('info')
+information.innerText = `${versions.chrome()}, ${versions.node()}, ${versions.electron()}`
+
+const selectFileBtn = document.getElementById('btn-select-file')
+selectFileBtn.addEventListener('click', async () => {
+  
+})
